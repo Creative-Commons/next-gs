51f1ad12121efa477591550c0e4520eb35ac918a
Keane Pereira <keane3pereira@gmail.com>
2021-04-07 12:22:56 +0530

    landing

--- components/Landing/Land1/index.js
+++ components/Landing/Land1/index.js
@@ -6,13 +6,16 @@ import {
 
 import { Link as Scroll } from 'react-scroll';
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import Tilt from "react-tilt";
 
 
 export default function Header ({classes}) {
     return (
         <div className={classes.root, classes.top_root} id="land-1">
-            <Container className={classes.root}>
-            </Container>
+            <Tilt>
+                <Container className={classes.root}>
+                </Container>
+            </Tilt>
             <Scroll className={classes.main} to="download-app" smooth={true}>
                 <IconButton className={classes.scroll_icon_light}>
                     <ExpandMoreIcon/>
