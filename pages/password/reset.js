ec038ebf5af6d590f2c44f91a436e7127256ae9c
Keane <keane3pereira@gmail.com>
2021-04-12 15:02:39 +0530

    reset password

--- pages/password/reset.js
+++ pages/password/reset.js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import API_BASE_URL from "../../constants";
 import { useState } from "react";
-import { Button, Fade, TextField, Container, Typography } from "@material-ui/core";
+import { Button, Fade, TextField, Container, Typography, Grid } from "@material-ui/core";
 
 
 
@@ -10,7 +10,8 @@ import { Button, Fade, TextField, Container, Typography } from "@material-ui/cor
 export default function ResetPasswordPage () {
     const { query } = useRouter();
     const [password, setPassword] = useState(undefined);
-    const [username, setUsername] = useState(undefined)
+    const [username, setUsername] = useState(undefined);
+    const [message, setMessage] = useState("");
 
     function resetPassword () {
         axios.post(
@@ -21,48 +22,59 @@ export default function ResetPasswordPage () {
                 "token": query.token
             }
         ).then((response) => {
-            console.log(response);
-        }).catch((error) => console.log(error));
+            console.log(response.data.message);
+        }).catch((error) => setMessage("Failed to reset password"));
     }
 
     return (
         <Fade in={true} timeout={500}>
             <Container style={{flexGrow: 1}}>
-                <Typography variant="h5">
-                  Reset Password
-                </Typography>
-                <div style={{paddingRight: 60, paddingLeft: 60}} >
-                    <TextField
-                      variant="outlined"
-                      margin="normal"
-                      required
-                      fullWidth
-                      id="username"
-                      label="Username"
-                      onChange={e => setUsername(e.target.value)}
-                      name="username"
-                      autoFocus
-                    />
-                    <TextField
-                      variant="outlined"
-                      margin="normal"
-                      required
-                      fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      onChange={e => setPassword(e.target.value)}
-                      id="password"
-                    />
-                    <Button
-                      fullWidth
-                      variant="contained"
-                      color="primary"
-                      onClick={() => {resetPassword()}}
-                    >
-                      Sign In
-                    </Button>
-                </div>
+                <Grid container direction="column" alignItems="center">
+                    <Grid item>
+                        <Typography variant="h3">
+                        Reset Password
+                        </Typography>
+                    </Grid>
+                    <Grid>
+                        <div style={{paddingRight: 60, paddingLeft: 60}} >
+                            <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            onChange={e => setUsername(e.target.value)}
+                            name="username"
+                            autoFocus
+                            />
+                            <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            onChange={e => setPassword(e.target.value)}
+                            id="password"
+                            />
+                            <Button
+                            fullWidth
+                            variant="contained"
+                            color="secondary"
+                            onClick={() => {resetPassword()}}
+                            >
+                            Reset Password
+                            </Button>
+                        </div>
+                    </Grid>
+                    <Grid item>
+                        <Typography>
+                            {message}
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Container>
         </Fade>
     )
